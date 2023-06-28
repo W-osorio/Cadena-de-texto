5503f7039c07b3d88d236bdd5a086252c021a2f3
W-osorio <111244407+W-osorio@users.noreply.github.com>
2023-06-27 23:23:24 -0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,20 @@
+let nombre = "willimder";
+let apellido = "Osorio";
+
+let estudiante =" willimder osorio ";
+
+let estudianteMayus
+    console.log(estudiante.toUpperCase())
+
+    let estudianteMinus
+    console.log(estudiante.toLowerCase())
+
+    console.log(estudiante.length)
+
+    console.log(estudiante.charAt(0))
+    
+    console.log(estudiante.charAt(11))
+
+    console.log(estudiante.trim().length)
+
+    console.log(estudiante.includes(nombre))
